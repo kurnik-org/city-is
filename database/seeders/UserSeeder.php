cba4cb5a5f4745d1a60ebba1ed6c34e1ec7b2fa8
Milan Tichavský <milantichavsky@seznam.cz>
2022-11-15 19:52:14 +0100

    Seed admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@smartcity.eu',
+            'password' => Hash::make('admin'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
