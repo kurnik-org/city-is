ec1692e1026b5725dc327c035b03febcf56b9413
Jan Valusek <janvalusek123@gmail.com>
2022-11-18 13:48:04 +0100

    Add create ticket screen and connect it with its model

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
@@ -23,7 +24,7 @@ class TicketController extends Controller
      */
     public function create()
     {
-        //
+        return view('tickets.create');
     }
 
     /**
@@ -34,7 +35,14 @@ class TicketController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required|string|max:128',
+            'description' => 'required|string'
+        ]);
+
+        $request->user()->tickets()->create($validated);
+
+        return redirect(route('tickets.index'));
     }
 
     /**
