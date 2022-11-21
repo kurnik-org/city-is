819c88783b8a8fd2693d2d7fb4d78abe19e88224
SpectruM9 <ivan.plachy9@gmail.com>
2022-11-21 16:11:21 +0100

    Add route to ticket view on click

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -19,29 +19,31 @@
         @if (Auth::user()->name == $ticket->author->name)
         <div class="pt-6">
             <div class="max-w-7xl mx-auto">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
-                        {{ $ticket->title }}
-                    </div>
-                    <div class="px-6 mt-6 text-gray-900">
+                <a href="{{ route('tickets.show', $ticket->id)}}">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="flex justify-between">
-                            <div>Author:</div>
-                            @if ($ticket->state == 0)
-                                State: Reported
-                            @else
-                                State: Work in progress
-                            @endif
+                            <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
+                                {{ $ticket->title }}                           
+                            </div>                            
+                        </div>
+                        <div class="px-6 mt-6 text-gray-900">
+                            <div class="flex justify-between">
+                                <div>Author:</div>
+                                @if ($ticket->state == 0)
+                                    State: Reported
+                                @else
+                                    State: Work in progress
+                                @endif
+                            </div>
+                        </div>
+                        <div class="px-6 text-gray-900 ">
+                            <div class="flex justify-between">
+                                <div class= "font-extrabold">{{ $ticket->author->name }}</div>
+                                <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
+                            </div>
                         </div>
                     </div>
-                    <div class="px-6 text-gray-900 ">
-                        <div class="flex justify-between">
-                            <div class= "font-extrabold">{{ $ticket->author->name }}</div>
-                            <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <a href="{{ route('tickets.show', $ticket->id)}}" class="btn btn-primary">Detail</a>
-                    </div>
-                </div>
+                </a>
             </div>
         </div>
         @endif
@@ -61,26 +63,29 @@
         @if (Auth::user()->name != $ticket->author->name)
         <div class="pt-6">
             <div class="max-w-7xl mx-auto">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
-                        {{ $ticket->title }}
-                    </div>
-                    <div class="px-6 mt-6 text-gray-900">
-                        <div class="flex justify-between">
-                            <div>Author:</div>
-                            @if ($ticket->state == 0)
-                                State: Reported
-                            @else
-                                State: Work in progress
-                            @endif
+                <a href="{{ route('tickets.show', $ticket->id)}}">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
+                            {{ $ticket->title }}
+                        </div>
+                        <div class="px-6 mt-6 text-gray-900">
+                            <div class="flex justify-between">
+                                <div>Author:</div>
+                                @if ($ticket->state == 0)
+                                    State: Reported
+                                @else
+                                    State: Work in progress
+                                @endif
+                            </div>
+                        </div>
+                        <div class="px-6 text-gray-900 ">
+                            <div class="flex justify-between">
+                                <div class= "font-extrabold">{{ $ticket->author->name }}</div>
+                                <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
+                            </div>
                         </div>
                     </div>
-                    <div class="px-6 text-gray-900 ">
-                        <div class="flex justify-between">
-                            <div class= "font-extrabold">{{ $ticket->author->name }}</div>
-                            <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
-                    </div>
-                </div>
+                </a>
             </div>
         </div>
         @endif
