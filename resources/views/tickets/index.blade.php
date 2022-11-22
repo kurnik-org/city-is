c5a5ad539ec2555f294a48493f1b333a2f7b993a
Jan Valusek <janvalusek123@gmail.com>
2022-11-22 14:35:47 +0100

    Create file for Ticket index item html

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -17,31 +17,7 @@
         </h1>
         @foreach ($tickets as $ticket)
         @if (Auth::user()->name == $ticket->author->name)
-        <div class="pt-6">
-            <div class="max-w-7xl mx-auto">
-                <a href="{{ route('tickets.show', $ticket->id)}}">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="flex justify-between">
-                            <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
-                                {{ $ticket->title }}
-                            </div>
-                        </div>
-                        <div class="px-6 mt-6 text-gray-900">
-                            <div class="flex justify-between">
-                                <div>Author:</div>
-                                State: {{ $ticket->state->user_friendly_string() }}
-                            </div>
-                        </div>
-                        <div class="px-6 text-gray-900 ">
-                            <div class="flex justify-between">
-                                <div class= "font-extrabold">{{ $ticket->author->name }}</div>
-                                <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
+            @include('common.ticket_index_item', ['ticket' => $ticket])
         @endif
         @endforeach
         @endif
@@ -57,29 +33,7 @@
         @endif
         @foreach ($tickets as $ticket)
         @if (Auth::user()->name != $ticket->author->name)
-        <div class="pt-6">
-            <div class="max-w-7xl mx-auto">
-                <a href="{{ route('tickets.show', $ticket->id)}}">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="pt-6 text-xl text-gray-900 px-6 col-span-2">
-                            {{ $ticket->title }}
-                        </div>
-                        <div class="px-6 mt-6 text-gray-900">
-                            <div class="flex justify-between">
-                                <div>Author:</div>
-                                State: {{ $ticket->state->user_friendly_string() }}
-                            </div>
-                        </div>
-                        <div class="px-6 text-gray-900 ">
-                            <div class="flex justify-between">
-                                <div class= "font-extrabold">{{ $ticket->author->name }}</div>
-                                <div class="mb-6">Last updated: {{ $ticket->updated_at }}</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
+            @include('common.ticket_index_item', ['ticket' => $ticket])
         @endif
         @endforeach
         @endif
